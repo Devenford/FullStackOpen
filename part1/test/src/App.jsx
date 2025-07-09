d7013d8b46c8dc0245e89cacf68dfc42aec72deb
Devenford <khaushldev.ls@gmail.com>
2025-07-09 20:02:49 +0530

    update

--- part1/test/src/App.jsx
+++ part1/test/src/App.jsx
@@ -1,26 +1,7 @@
-const Hello = ({name, age}) => {
-  const birthYear = () =>  new Date().getFullYear() - age
-
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So, you were born in {birthYear()}</p>
-    </div>
-  )
-}
-
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
+    <div>{counter}</div>
   )
 }
 
