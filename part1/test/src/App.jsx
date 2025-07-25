f19a5a237b2e0bb9ba2e4a20bfb6ac0ad68bc70f
Devenford <khaushldev.ls@gmail.com>
2025-07-25 21:57:38 +0530

    Update App.jsx

--- part1/test/src/App.jsx
+++ part1/test/src/App.jsx
@@ -3,10 +3,15 @@ import {useState} from 'react'
 const App = () => {
   const[value, setValue] = useState(10)
 
+  const handleClick = () => {
+    console.log('clicked the button')
+    setValue(0)
+  }
+
   return (
     <div>
       {value}
-      <button onClick={() => setValue(0)}>reset to zero</button>
+      <button onClick={handleClick}>button</button>
     </div>
   )
 }
