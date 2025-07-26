be8f058a893cf8e29b611e036549d3b9ef75ebd4
Devenford <khaushldev.ls@gmail.com>
2025-07-27 00:06:49 +0530

    Update App.jsx

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,10 +1,18 @@
-
+import { useState } from "react"
 
 const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      hello
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h2>Statistics</h2>
+      <p>good {good}<br />neutral {neutral}<br />bad {bad}</p>
     </div>
   )
 }
