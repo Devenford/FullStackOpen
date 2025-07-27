be7ee01973acb38dcbd701c8c859f5da55ae5e9e
Devenford <khaushldev.ls@gmail.com>
2025-07-27 11:22:55 +0530

    Update App.jsx

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,21 @@
 import { useState } from "react"
 
+const Statistics = (props) => {
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>good {props.good}
+        <br />neutral {props.neutral}
+        <br />bad {props.bad}
+        <br />all {props.good + props.neutral + props.bad}
+        <br />average {(props.good-props.bad)/(props.good + props.neutral + props.bad)}
+        <br />positive {props.good/(props.good  + props.neutral  + props.bad)}
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -11,14 +27,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h2>Statistics</h2>
-      <p>good {good}
-        <br />neutral {neutral}
-        <br />bad {bad}
-        <br />all {good + neutral + bad}
-        <br />average {(good-bad)/(good + neutral + bad)}
-        <br />positive {good/(good  + neutral  + bad)}
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
