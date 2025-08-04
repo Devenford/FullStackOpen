29ba36f616a1011cd2f6be50556fa74bd9d05dd5
Devenford <khaushldev.ls@gmail.com>
2025-08-04 23:19:54 +0530

    Update App.jsx

---
+++ part2/test/src/App.jsx
@@ -0,0 +1,45 @@
+var animals = [
+  {name: 'Fluffykins', species: 'rabbit'},
+  {name: 'Caro', species: 'dog'},
+  {name: 'Hamilton', species: 'dog'},
+  {name: 'Harold', species: 'fish'},
+  {name: 'Ursula', species: 'cat'},
+  {name: 'Jimmy', species: 'fish'}
+]
+
+var isDog = function(animal) {
+  return animals.species === 'dogs'
+}
+
+var dogs = animals.filter(isDog)
+
+
+
+
+
+/*
+var dogs = []
+for (var i=0; i<animals.length; i++) {
+  if(animals[i].species === 'dog') {
+    dogs.push(animals[i])
+  }
+}*/
+
+
+
+
+
+
+
+
+
+
+
+const App = () => {
+
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default App
