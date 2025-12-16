21e816085305f9bdf0c327efbfedb7d60c49a431
Devenford <khaushldev.ls@gmail.com>
2025-12-16 17:18:30 +0530

    part 2 c

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,24 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Filter from './components/FilterPeople'
 import PersonForm from './components/PersonForm'
 import People from './components/DisplayPeople'
+import axios from 'axios'
 
 const App = () => {
-  const [people, setPeople] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [people, setPeople] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameFilter, setNameFilter] = useState('')
+
+  const hook = () => {
+    axios
+        .get("http://localhost:3001/persons")
+        .then(response => {
+          setPeople(response.data)
+        })
+  }
+
+  useEffect(hook, [])
 
   const peopleToShow = people.filter(person => (person.name.toLowerCase()).includes(nameFilter.toLowerCase()))
 
