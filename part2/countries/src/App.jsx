d63499c9c343f735c78197378c79190f341fe5a5
Devenford <khaushldev.ls@gmail.com>
2025-12-25 21:09:23 +0530

    part 2 e

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,10 +1,28 @@
 import {useState, useEffect} from 'react'
+import dataService from './services/retrieveData'
+import DisplayCountries from './components/DisplayCountries'
 
 const App = () => {
+    const [countries, setCountries] = useState(null)
+    const [inputCountry, setInputCountry] = useState('')
+
+    useEffect(() => {
+        dataService
+            .getAll()
+            .then(fetchedData => {
+                setCountries(fetchedData)
+            })
+    }, [])
+
+    const handleInput = (event) => {
+        setInputCountry(event.target.value)
+    }
 
     return (
         <div>
-
+            find countries:
+            <input onChange={handleInput}/>
+            <DisplayCountries countries={countries} inputCountry={inputCountry} setInputCountry={setInputCountry}/>
         </div>
     )
 }
---
+++ part2/countries/src/components/DisplayCountries.jsx
@@ -0,0 +1,64 @@
+const DisplayCapital = ({capitals}) => {
+    return (
+        <ul>
+            {capitals.map((capital, index) => <li key={index}>{capital}</li>)}
+        </ul>
+    )
+}
+
+const DisplayLanguages = ({languages}) => {
+    const entries = Object.entries(languages)
+
+    return (
+        <ul>
+            {entries.map(([key, value])  => <li key={key}>{value}</li>)}
+        </ul>
+    )
+}
+
+const DisplayCountries = ({countries, inputCountry, setInputCountry}) => {
+    if (countries===null || inputCountry==='') {
+        return null
+    }
+    
+    const countriesToDisplay = countries.filter(country =>
+        country.name.common.toLowerCase().startsWith(inputCountry.toLowerCase()))
+
+    const count = countriesToDisplay.length
+
+    if (count>10) {
+        return <div>
+            Too many matches, specify another filter
+        </div>
+    }
+
+    if(count>1) {
+        return (
+            <ul>
+                {countriesToDisplay.map(country =>
+                    <li key={country.cca3}>{country.name.common}</li>
+                )}
+            </ul>
+        )
+    }
+
+    if (count===1) {
+        const selectedCountry = countriesToDisplay[0]
+
+        return (
+            <div>
+                <h1>{selectedCountry.name.common}</h1>
+                <h2>Capital:</h2> <DisplayCapital capitals={selectedCountry.capital}/>
+                <h2>Area:</h2> {selectedCountry.area}
+
+                <h2>Languages:</h2>
+                <DisplayLanguages languages={selectedCountry.languages}/>
+                <img src={selectedCountry.flags.png}/>
+            </div>
+        )
+    }
+
+    return null
+}
+
+export default DisplayCountries
