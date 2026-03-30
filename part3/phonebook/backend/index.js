86689684ae0069a30681b493d239e08f47a41a2a
Devenford <khaushldev.ls@gmail.com>
2026-03-31 01:16:44 +0530

    completed part 3 d 3.22

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 const morgan = require('morgan')
-morgan.token('entry', (request, response) => {
+morgan.token('entry', (request) => {
   return JSON.stringify(request.body)})
 
 app.use(express.json())
@@ -15,7 +15,7 @@ const Entry = require('./models/entry.js')
 
 // route handlers:
 app.get('/api/persons', (request, response, next) => {
-    Entry.find({})
+  Entry.find({})
     .then(entries => {
       response.json(entries)
     })
@@ -24,72 +24,72 @@ app.get('/api/persons', (request, response, next) => {
 
 app.get('/api/persons/:id', (request, response, next) => {
   Entry.findById(request.params.id)
-  .then(entry => {
-    if(entry) {
-      response.json(entry)
-    }
-    else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+    .then(entry => {
+      if(entry) {
+        response.json(entry)
+      }
+      else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/info', (request, response, next) => {
   const time = new Date()
   Entry.find({})
-  .then(entries => {
-    response.send(`<div>
+    .then(entries => {
+      response.send(`<div>
     <p>Phonebook has info for ${entries.length} people</p>
     <p>${time}</p>
     </div>`)
-  })
-  .catch(error => next(error))
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-  const {name, number} = request.body
+  const { name, number } = request.body
 
-  Entry.find({name: name})
-  .then(entries => {
-    let entry
-    if(entries.length !== 0) {
-      entry = entries[0]
-      entry.name = name
-      entry.number = number
-    }
-    else {
-      entry = new Entry({
-        name: name,
-        number: number
-      })
-    }
+  Entry.find({ name: name })
+    .then(entries => {
+      let entry
+      if(entries.length !== 0) {
+        entry = entries[0]
+        entry.name = name
+        entry.number = number
+      }
+      else {
+        entry = new Entry({
+          name: name,
+          number: number
+        })
+      }
 
-    return entry.save().then(savedEntry => response.json(savedEntry))
-  })
-  .catch(error => next(error))
+      return entry.save().then(savedEntry => response.json(savedEntry))
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Entry.findByIdAndDelete(request.params.id)
-  .then(result => {
-    if(!result) {
-      return response.status(404).end()
-    }
+    .then(result => {
+      if(!result) {
+        return response.status(404).end()
+      }
 
-    response.status(204).end()
-  })
-  .catch(error => next(error))
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if(error.name === 'CastError') {
-    return response.status(400).json({error: 'malformatted id'})
+    return response.status(400).json({ error: 'malformatted id' })
   }
   else if(error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message})
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -99,5 +99,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- part3/phonebook/backend/models/entry.js
+++ part3/phonebook/backend/models/entry.js
@@ -3,39 +3,39 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
-mongoose.connect(url, {family:4})
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+mongoose.connect(url, { family:4 })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const entrySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(val) {
+        return /^\d{2,3}-\d+$/.test(val)
+      },
+      message: 'Phone number must be in format XX-XXXXX or XXX-XXXXX'
     },
-    number: {
-        type: String,
-        minLength: 8,
-        validate: {
-            validator: function(val) {
-                return /^\d{2,3}-\d+$/.test(val)
-            },
-            message: 'Phone number must be in format XX-XXXXX or XXX-XXXXX'
-        },
-        required: true
-    }
+    required: true
+  }
 })
 
 entrySchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Entry', entrySchema)
