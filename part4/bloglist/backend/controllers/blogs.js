b6dd1fe55c827ddbfc7c7248d8956445c782c31c
Devenford <khaushldev.ls@gmail.com>
2026-06-03 04:01:11 +0530

    completed 4.14 added all tests

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -1,18 +1,53 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+  if (blog) {
+    response.json(blog)
+  }
+  else {
+    response.status(404).end()
+  }
+})
+
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+  const result = await blog.save()
+  response.status(201).json(result)
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body
+  const blogToBeUpdated = await Blog.findById(request.params.id)
+
+  if (!blogToBeUpdated) {
+    return response.status(404).end()
+  }
+
+  blogToBeUpdated.title = title
+  blogToBeUpdated.author = author
+  blogToBeUpdated.url = url
+  blogToBeUpdated.likes = likes
+
+  const updatedBlog = await blogToBeUpdated.save()
+  response.status(200).json(updatedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const result = await Blog.findByIdAndDelete(request.params.id)
+
+  if (!result) {
+    return response.status(404).end()
+  }
+
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
--- part4/bloglist/backend/tests/blog_api.test.js
+++ part4/bloglist/backend/tests/blog_api.test.js
@@ -8,7 +8,7 @@ const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-describe('When a few blogs are initially saved', async () => {
+describe('When a few blogs are initially saved', () => {
   beforeEach(async () => {
     await Blog.deleteMany({})
     await Blog.insertMany(helper.initialBlogs)
@@ -24,6 +24,117 @@ describe('When a few blogs are initially saved', async () => {
   test('all blogs are returned', async () => {
     const response = await api.get('/api/blogs')
     assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+
+  test('blogs contain id property, not _id', async () => {
+    const response = await api.get('/api/blogs')
+    assert(Object.keys(response.body[0]).includes('id'))
+    assert(!Object.keys(response.body[0]).includes('_id'))
+  })
+
+  describe('addition of a new blog post', () => {
+    test('succeeds with valid data', async () => {
+      const newBlog = {
+        title: 'Star Wars',
+        author: 'George Lucas',
+        url: 'http://starwars.com/revenge-of-the-sith',
+        likes: 2,
+      }
+
+      const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+      const savedBlog = await api
+        .get(`/api/blogs/${response.body.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      assert.deepStrictEqual(savedBlog.body, response.body)
+    })
+
+    test('value of likes property is 0 by default', async () => {
+      const newBlog = {
+        title: 'Star Wars',
+        author: 'George Lucas',
+        url: 'http://starwars.com/revenge-of-the-sith',
+      }
+
+      const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      assert.strictEqual(response.body.likes, 0)
+    })
+
+    test('fails with status code 400 if the title is missing', async () => {
+      const newBlog = {
+        author: 'George Lucas',
+        url: 'http://starwars.com/revenge-of-the-sith',
+        likes: 2,
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+    })
+
+    test('fails with status code 400 if the url is missing', async () => {
+      const newBlog = {
+        title: 'Star Wars',
+        author: 'George Lucas',
+        likes: 2,
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+    })
+  })
+
+  describe('updating information of an individual blog', () => {
+    test('succeeds with status code 200 if the id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToBeUpdated = blogsAtStart[0]
+      const updatedBlogData = {
+        ...blogToBeUpdated,
+        likes: blogToBeUpdated.likes + 1
+      }
+
+      const updatedBlog = await api
+        .put(`/api/blogs/${updatedBlogData.id}`)
+        .send(updatedBlogData)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      assert.deepStrictEqual(updatedBlog.body, updatedBlogData)
+    })
+  })
+
+  describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if the id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      const ids = blogsAtEnd.map(blog => blog.id)
+      assert(!ids.includes(blogToDelete.id))
+
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+    })
   })
 })
 
